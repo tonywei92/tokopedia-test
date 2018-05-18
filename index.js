1225ac5b5f2f4cf0917fecb989fd9c6376f95d67
Tony <tonywei92@gmail.com>
2018-05-18 22:21:56 +0700

    fix browser fallback

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const path = require('path')
+var fallback = require('express-history-api-fallback');
+var express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000
 
 const app = express();
+var root = path.resolve(__dirname);
 
-app.use(express.static('./'));
-app.listen(PORT, () => console.log('Example app listening on port 3000!'))
+app.use(express.static(__dirname));
+app.use(fallback('index.html', { root: root }));
+
+app.listen(PORT, () => console.log('The server listening on port ' + PORT +'!'));
